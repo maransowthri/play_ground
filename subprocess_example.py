7c196869947e6eef9038723ce6283720b095e2e4
Maran Sowthri Kalailingam <kmaran@athenahealth.com>
2020-07-21 08:55:56 +0530

    Added trposphere methods into playground

--- subprocess_example.py
+++ subprocess_example.py
@@ -1,9 +1,6 @@
 import subprocess
 import logging
 
-LOG_FORMAT = "%(levelname)s %(asctime)s - (%message)"
-logging.basicConfig(filename='subprocess.log', filemode='w', format=LOG_FORMAT, level=logging.DEBUG)
-logger_obj = logging.getLogger()
 p1 = subprocess.run(['python', '--version'], shell=True, capture_output=True, text=True)
 print(type(p1.stdout))
 print(p1.stdout)
